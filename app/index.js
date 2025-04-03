a6bbe149c01dd514d465016ea45f32a1aa521d32
Julio23F <juliofaralahy23@gmail.com>
2025-04-03 14:48:51 +0300

    feat: change architecture

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { ThemedText } from '@/components/ThemedText';
+import { Box } from '@/components/Box';
+
+const index = () => {
+  return (
+    <Box>
+        <ThemedText type="title">Welcome!</ThemedText>
+    </Box>
+  )
+}
+
+export default index
