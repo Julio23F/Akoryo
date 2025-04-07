357de2aa53e9a521156a94403cb3ebc754bc4782
Julio23F <juliofaralahy23@gmail.com>
2025-04-07 16:28:23 +0300

    feat: add authContexte

---
+++ context/authContext.js
@@ -0,0 +1,55 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [isAuthentificated, setIsAuthentificated] = useState(undefined);
+
+    useEffect(() => {
+
+    }, []);
+
+    const login = async (email, password) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    const register = async (username, email, password) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    const logout = async () => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+
+    return (
+        <AuthContext.Provider value={{user, isAuthentificated, login, register, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuh = () => {
+    const value = useContext(AuthContext);
+
+
+    if(!value) {
+        throw new Error("useAuth must be wrapped inside AuthContextProvider");
+    }
+
+    return value;
+}
